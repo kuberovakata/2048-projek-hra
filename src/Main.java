5d551c7ffa47838d74d13d8e0ea57931848710d0
Katka <kuberova.kata@gmail.cz>
2026-05-30 23:54:21 +0200

    zavedení hlavního menu

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+import javax.swing.*;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        HlavniMenu hlavnimenu = new HlavniMenu();
+        SwingUtilities.invokeLater(() -> {
+            hlavnimenu.setVisible(true);});
 
     }
 }
