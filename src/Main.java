b68267c1a66b7939697ac76ec9f4b1231121aa2d
Katka <kuberova.kata@gmail.cz>
2026-05-31 19:09:00 +0200

    zhotovění

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 
 public class Main {
     public static void main(String[] args) {
-
         /**
          * Hlavní metoda, vstupní bod programu.
          * Vytvoření hlavního menu, ze kterého se následně spouští samotná hra.
